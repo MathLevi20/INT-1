503cdecd86f6484b0740995f09d3a80340790513
Matheus Levi <84115454+MathLevi20@users.noreply.github.com>
2022-02-22 23:49:10 -0300

    Atividade 01 <->"Finalizada"

---
+++ Q02.js
@@ -0,0 +1,11 @@
+﻿const axios = require('axios');
+
+axios.get('https://progressivebot.netlify.app/')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+  });
---
+++ Q03.js
@@ -0,0 +1,27 @@
+﻿'use strict'
+
+const fs = require("fs")
+const path = require("path")
+const axios = require("axios")
+
+async function download() {
+
+    const url = 'https://s3.amazonaws.com/cdn.chickenorpasta.com.br/content/uploads/2018/07/27144837/Akira-Kaneda-1013x620.jpg'
+    const imagepath = path.resolve(__dirname, 'files', 'image.jpg')
+    const writer = fs.createWriteStream(imagepath)
+    const response = await axios({
+        method: 'GET',
+        url: url,
+        responseType: 'stream'
+    })
+
+
+    response.data.pipe(writer)
+    
+    return new Promise((resolve, reject) => {
+        writer.on('finish', resolve)
+        writer.on('erro', reject)
+    })
+
+}
+download()
