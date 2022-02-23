ea81ffea182840c890a6ce2d1a2c02a94ded686f
Matheus Levi <84115454+MathLevi20@users.noreply.github.com>
2022-02-23 00:00:01 -0300

    Atividade 01 <-> "Finalizada"

---
+++ Q04.js
@@ -0,0 +1,16 @@
+﻿axios = require("axios")
+let url = 'https://pt.simplesite.com/'
+
+axios.get(`${url}`).then(function(response){
+    result = `${response.data }`
+        Seach_html(result)})
+
+    function Seach_html(html){
+        html = html.split('<a href');
+
+            for(i in html){
+                links = html[i].split('>')
+                    links = links[0].split('"')
+                        console.log(links[1])
+    }
+}
